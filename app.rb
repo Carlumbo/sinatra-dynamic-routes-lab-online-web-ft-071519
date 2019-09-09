780279cdf3aa0ca73593957b98d6ea3476096f19
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-09 22:43:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ class App < Sinatra::Base
     "#{params[:phrase]}"*params[:number].to_i
   end 
   
-  get 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    "#{params[:word1]}"
     
 end
